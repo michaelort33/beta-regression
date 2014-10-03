476ea19aebbafe2f26d4972104e35dd310232b3d
Brent Pedersen <bpederse@gmail.com>
2014-10-03 13:16:52 -0600

    add link and link_phi

--- betareg.py
+++ betareg.py
@@ -3,18 +3,20 @@ from scipy.special import gammaln as lgamma
 from statsmodels.base.model import GenericLikelihoodModel
 from statsmodels.api import GLM
 from statsmodels.genmod.families import Binomial
+import statsmodels.api as sm
 
 #see http://cran.r-project.org/web/packages/betareg/vignettes/betareg-ext.pdf
-
-def ilogit(a):
-    return 1 / (1. + np.exp(-a))
-
-def logit(a):
-    return np.log(p / (1. - p))
+class Logit(sm.families.links.Logit):
+    def inverse(self, z):
+        return 1 / (1. + np.exp(-z))
 
 class BetaReg(GenericLikelihoodModel):
-    def __init__(self, endog, exog, Z=None, **kwds):
+    def __init__(self, endog, exog, Z=None, link=Logit(),
+            link_phi=sm.families.links.Log(), **kwds):
+
         super(BetaReg, self).__init__(endog, exog, **kwds)
+        self.link = link
+        self.link_phi = link_phi
         # how to set default Z?
         if Z is None:
             self.Z = np.ones((self.endog.shape[0], 1), dtype='f')
@@ -25,7 +27,8 @@ class BetaReg(GenericLikelihoodModel):
     def nloglikeobs(self, params):
         return -self._ll_br(self.endog, self.exog, self.Z, params)
 
-    def fit(self, start_params=None, maxiter=1000000, maxfun=50000, **kwds):
+    def fit(self, start_params=None, maxiter=1000000, maxfun=50000,
+            method='bfgs', **kwds):
         if start_params is None:
             start_params = GLM(self.endog, self.exog, family=Binomial()).fit().params
             start_params = np.append(start_params, [0.5] * self.Z.shape[1])
@@ -35,6 +38,7 @@ class BetaReg(GenericLikelihoodModel):
         return super(BetaReg, self).fit(start_params=start_params,
                                              maxiter=maxiter,
                                              maxfun=maxfun,
+                                             method=method,
                                              **kwds)
     def _ll_br(self, y, X, Z, params):
         nz = self.Z.shape[1]
@@ -42,8 +46,8 @@ class BetaReg(GenericLikelihoodModel):
         Xparams = params[:-nz]
         Zparams = params[-nz:]
 
-        mu = ilogit(np.dot(X, Xparams))
-        phi = np.exp(np.dot(Z, Zparams))
+        mu = self.link.inverse(np.dot(X, Xparams))
+        phi = self.link_phi.inverse(np.dot(Z, Zparams))
 
         ll = lgamma(phi) - lgamma(mu * phi) - lgamma((1 - mu) * phi) \
                 + (mu * phi - 1) * np.log(y) + (((1 - mu) * phi) - 1) * np.log(1 - y)
